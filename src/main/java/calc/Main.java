afa4d0b5b13d4251de3cd979a06de44ffcba18d2
DanloxBR <danlox0602@gmail.com>
2026-02-25 18:05:54 -0300

    feat: adiciona projeto Calculadora Java Swing

---
+++ src/main/java/calc/Main.java
@@ -0,0 +1,9 @@
+package calc;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Calculator());
+    }
+}
